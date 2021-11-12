2ae87880f1e044443954952c048ac9351e19466d
sciug <gaetano.sciurti@libero.it>
2021-11-12 11:25:45 +0100

    add imgs

---
+++ src/components/Product.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+      <img :src="thumb" alt="">
+      <h3>{{series}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        thumb: String,
+        series: String
+    }
+
+}
+</script>
+
+<style>
+
+</style>
